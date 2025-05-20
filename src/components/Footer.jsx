964e0849d8b3a1895c206f9fb62ef357be208999
lucioacri <lucioacri@gmail.com>
2025-05-20 17:16:22 +0200

    add footer image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,106 +4,113 @@ export default function Footer() {
       <section id="footer-navbar">
         <div className="container">
           <div className="row">
-            <div className="col-1-6">
-              <h5>DC COMICS</h5>
-              <nav>
-                <ul>
-                  <li>
-                    <a href="#">Characters</a>
-                  </li>
-                  <li>
-                    <a href="#">Comics</a>
-                  </li>
-                  <li>
-                    <a href="#">Movies</a>
-                  </li>
-                  <li>
-                    <a href="#">TV</a>
-                  </li>
-                  <li>
-                    <a href="#">Games</a>
-                  </li>
-                  <li>
-                    <a href="#">Videos</a>
-                  </li>
-                  <li>
-                    <a href="#">News</a>
-                  </li>
-                </ul>
-              </nav>
-              <h5>SHOP</h5>
-              <nav>
-                <ul>
-                  <li>
-                    <a href="#">Shop DC</a>
-                  </li>
-                  <li>
-                    <a href="#"></a>Shop DC Collectibles
-                  </li>
-                </ul>
-              </nav>
+            <div className="col-1-2">
+              <div className="row">
+                <div className="col-1-3 links-list">
+                  <h5>DC COMICS</h5>
+                  <nav>
+                    <ul>
+                      <li>
+                        <a href="#">Characters</a>
+                      </li>
+                      <li>
+                        <a href="#">Comics</a>
+                      </li>
+                      <li>
+                        <a href="#">Movies</a>
+                      </li>
+                      <li>
+                        <a href="#">TV</a>
+                      </li>
+                      <li>
+                        <a href="#">Games</a>
+                      </li>
+                      <li>
+                        <a href="#">Videos</a>
+                      </li>
+                      <li>
+                        <a href="#">News</a>
+                      </li>
+                    </ul>
+                  </nav>
+                  <h5>SHOP</h5>
+                  <nav>
+                    <ul>
+                      <li>
+                        <a href="#">Shop DC</a>
+                      </li>
+                      <li>
+                        <a href="#"></a>Shop DC Collectibles
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="col-1-3 links-list">
+                  <h5>DC</h5>
+                  <nav>
+                    <ul>
+                      <li>
+                        <a href="#">Term Of Use</a>
+                      </li>
+                      <li>
+                        <a href="#">Privacy Policy (New)</a>
+                      </li>
+                      <li>
+                        <a href="#">Ad Choices</a>
+                      </li>
+                      <li>
+                        <a href="#">Advertising</a>
+                      </li>
+                      <li>
+                        <a href="#">Jobs</a>
+                      </li>
+                      <li>
+                        <a href="#">Subscriptions</a>
+                      </li>
+                      <li>
+                        <a href="#">Talent Workshops</a>
+                      </li>
+                      <li>
+                        <a href="#">CPSC Certificates</a>
+                      </li>
+                      <li>
+                        <a href="#">Ratings</a>
+                      </li>
+                      <li>
+                        <a href="#">Shop Help</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact Us</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+                <div className="col-1-3 links-list">
+                  <h5>SITES</h5>
+                  <nav>
+                    <ul>
+                      <li>
+                        <a href="#">DC</a>
+                      </li>
+                      <li>
+                        <a href="#">MAD Magazine</a>
+                      </li>
+                      <li>
+                        <a href="#">DC Kids</a>
+                      </li>
+                      <li>
+                        <a href="#">DC Universe</a>
+                      </li>
+                      <li>
+                        <a href="#">DC Power Visa</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
             </div>
-            <div className="col-1-6">
-              <h5>DC</h5>
-              <nav>
-                <ul>
-                  <li>
-                    <a href="#">Term Of Use</a>
-                  </li>
-                  <li>
-                    <a href="#">Privacy Policy (New)</a>
-                  </li>
-                  <li>
-                    <a href="#">Ad Choices</a>
-                  </li>
-                  <li>
-                    <a href="#">Advertising</a>
-                  </li>
-                  <li>
-                    <a href="#">Jobs</a>
-                  </li>
-                  <li>
-                    <a href="#">Subscriptions</a>
-                  </li>
-                  <li>
-                    <a href="#">Talent Workshops</a>
-                  </li>
-                  <li>
-                    <a href="#">CPSC Certificates</a>
-                  </li>
-                  <li>
-                    <a href="#">Ratings</a>
-                  </li>
-                  <li>
-                    <a href="#">Shop Help</a>
-                  </li>
-                  <li>
-                    <a href="#">Contact Us</a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div className="col-1-6">
-              <h5>SITES</h5>
-              <nav>
-                <ul>
-                  <li>
-                    <a href="#">DC</a>
-                  </li>
-                  <li>
-                    <a href="#">MAD Magazine</a>
-                  </li>
-                  <li>
-                    <a href="#">DC Kids</a>
-                  </li>
-                  <li>
-                    <a href="#">DC Universe</a>
-                  </li>
-                  <li>
-                    <a href="#">DC Power Visa</a>
-                  </li>
-                </ul>
-              </nav>
+            <div className="col-1-2 logo-container">
+              <img className="big-logo" src="./dc-logo-bg.png" alt="Logo DC" />
             </div>
           </div>
         </div>
