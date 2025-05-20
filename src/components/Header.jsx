9ffc61d558ed1a766ac1af22fcc9bf0ec7d08601
lucioacri <lucioacri@gmail.com>
2025-05-20 15:38:29 +0200

    add main's sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,44 @@ export default function Header() {
           </div>
           <nav>
             <ul>
-              <li>CHARACTERS</li>
-              <li>COMICS</li>
-              <li>MOVIES</li>
-              <li>TV</li>
-              <li>GAMES</li>
-              <li>COLLECTIBLES</li>
-              <li>VIDEOS</li>
-              <li>FANS</li>
-              <li>NEWS</li>
-              <li>SHOP</li>
+              <li>
+                <a href="#">CHARACTERS</a>
+              </li>
+              <li>
+                <a href="#">COMICS</a>
+              </li>
+
+              <li>
+                <a href="#">MOVIES</a>
+              </li>
+
+              <li>
+                <a href="#">TV</a>
+              </li>
+
+              <li>
+                <a href="#">GAMES</a>
+              </li>
+
+              <li>
+                <a href="#">COLLECTIBLES</a>
+              </li>
+
+              <li>
+                <a href="#">VIDEOS</a>
+              </li>
+
+              <li>
+                <a href="#">FANS</a>
+              </li>
+
+              <li>
+                <a href="#">NEWS</a>
+              </li>
+
+              <li>
+                <a href="#">SHOP</a>
+              </li>
             </ul>
           </nav>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,36 @@
 export default function Main() {
-  return <h1>hello</h1>;
+  return (
+    <main>
+      <section id="content-main">
+        <div className="container">
+          <div className="row">
+            <h2>-- Content goes here --</h2>
+          </div>
+        </div>
+      </section>
+      <section id="merchandise-main">
+        <div className="container">
+          <nav className="merchandise-navbar">
+            <ul>
+              <li>
+                <a href="#">DIGITAL COMICS</a>
+              </li>
+              <li>
+                <a href="#">DC MERCHANDISE</a>
+              </li>
+              <li>
+                <a href="#">SUBSCRIPTIONS</a>
+              </li>
+              <li>
+                <a href="#">COMIC SHOP LOCATOR</a>
+              </li>
+              <li>
+                <a href="#">DC POWER VISA</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+    </main>
+  );
 }
