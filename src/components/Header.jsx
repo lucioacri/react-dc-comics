54e7baffd02d7db6bd134d853bcb61b2aa5293a3
lucioacri <lucioacri@gmail.com>
2025-05-20 15:13:53 +0200

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+export default function Header() {
+  return (
+    <header id="page-header">
+      <div className="container">
+        <div className="row">
+          <div className="col-1-6">
+            <img src="./dc-logo.png" alt="DC Logo" />
+          </div>
+          <nav>
+            <ul>
+              <li>CHARACTERS</li>
+              <li>COMICS</li>
+              <li>MOVIES</li>
+              <li>TV</li>
+              <li>GAMES</li>
+              <li>COLLECTIBLES</li>
+              <li>VIDEOS</li>
+              <li>FANS</li>
+              <li>NEWS</li>
+              <li>SHOP</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
